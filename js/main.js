ac175e76dff5b6299951e36af74ffb3bd05148aa
mab_gray <m2bbie@gmail.com>
2023-03-09 14:08:46 -0600

    Grabbed color from input tag. Applied it to styles

---
+++ js/main.js
@@ -0,0 +1,27 @@
+
+
+function getInputValue1(){
+    // Selecting the input element and get its value 
+    var inputVal = document.getElementById("myInput1").value;
+    var box1 = document.querySelector("#color1");
+  
+    box1.style.backgroundColor = inputVal;
+
+    // box1.style.backgroundColor(inputVal);
+
+
+
+   
+}  
+    // Displaying the value
+    function getInputValue2(){
+        // Selecting the input element and get its value 
+        var inputVal = document.getElementById("myInput2").value;
+    
+        var box2 = document.querySelector("#color2");
+    
+        box2.style.backgroundColor(inputVal);
+    
+    
+   
+    }  
