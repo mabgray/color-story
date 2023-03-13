7e1184bc3bae90ad19a1dfe344d3972bf756a045
mab_gray <m2bbie@gmail.com>
2023-03-13 07:18:11 -0500

    More event listeners

--- js/main.js
+++ js/main.js
@@ -1,27 +1,56 @@
 
-
-function getInputValue1(){
-    // Selecting the input element and get its value 
-    var inputVal = document.getElementById("myInput1").value;
-    var box1 = document.querySelector("#color1");
+function getInputValue(inputElement, querySelect){
+    // Selecting the input element and get its value ()
+    console.log(inputElement);
+    var inputVal = document.getElementById(inputElement).value;
   
-    box1.style.backgroundColor = inputVal;
+    console.log(inputVal);
+    var box = document.querySelector(querySelect);
+    console.log(querySelect);
+    console.log(box);
+    
+ inputVal = inputVal.replace(" ", "");
+    box.style.backgroundColor = inputVal;
+    
 
     // box1.style.backgroundColor(inputVal);
 
-
-
    
 }  
-    // Displaying the value
-    function getInputValue2(){
-        // Selecting the input element and get its value 
-        var inputVal = document.getElementById("myInput2").value;
+
+function addADot(){
+var newDot = document.createElement("div");
+   
+    newDot.classList.add("colorBlock");
+    newDot.textContent = "color"
+
+ var dotPosition = document.querySelector(".container");
+ dotPosition.appendChild(newDot);
+
+
+ //this adds the field to enter a color
+
+
+ var newColorField = document. createElement("input"); 
+ newColorField. setAttribute('type', 'text'); 
+ newColorField. setAttribute('onclick', 'getInputValue()'); 
+ newColorField. setAttribute('placeholder', 'enter a color name'); 
+ var newButton = document.createElement("button");
+
+ //Then you can use appendChild to append the created element to the desired parent element.
+ var fieldPosition = document.querySelector(".container");
+ fieldPosition.appendChild(newColorField);
+
+ var buttonPosition = document.querySelector(".container");
+buttonPosition.appendChild(newButton);
+
+}
+
+
     
-        var box2 = document.querySelector("#color2");
     
-        box2.style.backgroundColor(inputVal);
+       
     
     
    
-    }  
+    
